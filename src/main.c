e26fb9d883d24e465f39122b91bfa0745691e2ec
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-05-10 23:34:09 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <memory.h>
 #include "include/log.h"
 #include "include/AL/al.h"
 #include "include/AL/alc.h"
+#include "include/AL/efx.h"
+#include "include/AL/efx-presets.h"
 #include "include/sndfile.h"
 
 void init();
@@ -15,6 +17,8 @@ ALCdevice* device;
 ALCcontext* context;
 ALuint buffer[1];
 ALuint source[1];
+ALuint effect[1];
+ALuint auxiliary_effect_slot[1];
 enum failure_statuses {
     NORMAL,
     DEFAULT_DEVICE_OPEN_FAILURE,
@@ -22,6 +26,8 @@ enum failure_statuses {
     MAKE_CONTEXT_CURRENT_FAILURE,
     SOURCE_BUFFER_GENERATION_FAILURE,
     AUDIO_SOURCE_GENERATION_FAILURE,
+    AUDIO_EFFECT_GENERATION_FAILURE,
+    AUXILIARY_EFFECT_SLOT_GENERATION_FAILURE,
     BIND_BUFFER_TO_SOURCE_FAILURE,
     AUDIO_FILE_OPEN_FAILURE,
     ALLOCATE_MEMORY_TO_DATA_BUFFER_FAILURE,
@@ -51,7 +57,9 @@ void init() {
         quit(DEFAULT_DEVICE_OPEN_FAILURE);
     }
 
-    context = alcCreateContext(device, NULL);
+    ALCint attr_list[2] = { ALC_FREQUENCY, 44100 };
+
+    context = alcCreateContext(device, attr_list);
 
     if (context == NULL) {
         log_error("Failed to create context");
@@ -83,12 +91,63 @@ void init() {
 
         quit(AUDIO_SOURCE_GENERATION_FAILURE);
     }
+
+    alGenEffects(1, effect);
+
+    if (alGetError() != AL_NO_ERROR) {
+        log_error("Failed to generate effect");
+
+        quit(AUDIO_EFFECT_GENERATION_FAILURE);
+    }
+
+    // Set reverb sound effect
+    alEffecti(effect[0], AL_EFFECT_TYPE, AL_EFFECT_EAXREVERB);
+
+    EFXEAXREVERBPROPERTIES reverb_preset = EFX_REVERB_PRESET_DRUGGED;
+
+    // Set reverb properties
+    alEffectf(effect[0], AL_EAXREVERB_DENSITY, reverb_preset.flDensity);
+    alEffectf(effect[0], AL_EAXREVERB_DIFFUSION, reverb_preset.flDiffusion);
+    alEffectf(effect[0], AL_EAXREVERB_GAIN, reverb_preset.flGain);
+    alEffectf(effect[0], AL_EAXREVERB_GAINHF, reverb_preset.flGainHF);
+    alEffectf(effect[0], AL_EAXREVERB_GAINLF, reverb_preset.flGainLF);
+    alEffectf(effect[0], AL_EAXREVERB_DECAY_TIME, reverb_preset.flDecayTime);
+    alEffectf(effect[0], AL_EAXREVERB_DECAY_HFRATIO, reverb_preset.flDecayHFRatio);
+    alEffectf(effect[0], AL_EAXREVERB_DECAY_LFRATIO, reverb_preset.flDecayLFRatio);
+    alEffectf(effect[0], AL_EAXREVERB_REFLECTIONS_GAIN, reverb_preset.flReflectionsGain);
+    alEffectf(effect[0], AL_EAXREVERB_REFLECTIONS_DELAY, reverb_preset.flReflectionsDelay);
+    alEffectfv(effect[0], AL_EAXREVERB_REFLECTIONS_PAN, reverb_preset.flReflectionsPan);
+    alEffectf(effect[0], AL_EAXREVERB_LATE_REVERB_GAIN, reverb_preset.flLateReverbGain);
+    alEffectf(effect[0], AL_EAXREVERB_LATE_REVERB_DELAY, reverb_preset.flLateReverbDelay);
+    alEffectfv(effect[0], AL_EAXREVERB_LATE_REVERB_PAN,  reverb_preset.flLateReverbPan);
+    alEffectf(effect[0], AL_EAXREVERB_ECHO_TIME, reverb_preset.flEchoTime);
+    alEffectf(effect[0], AL_EAXREVERB_ECHO_DEPTH, reverb_preset.flEchoDepth);
+    alEffectf(effect[0], AL_EAXREVERB_MODULATION_TIME, reverb_preset.flModulationTime);
+    alEffectf(effect[0], AL_EAXREVERB_MODULATION_DEPTH, reverb_preset.flModulationDepth);
+    alEffectf(effect[0], AL_EAXREVERB_AIR_ABSORPTION_GAINHF, reverb_preset.flAirAbsorptionGainHF);
+    alEffectf(effect[0], AL_EAXREVERB_HFREFERENCE, reverb_preset.flHFReference);
+    alEffectf(effect[0], AL_EAXREVERB_LFREFERENCE, reverb_preset.flLFReference);
+    alEffectf(effect[0], AL_EAXREVERB_ROOM_ROLLOFF_FACTOR, reverb_preset.flRoomRolloffFactor);
+    alEffecti(effect[0], AL_EAXREVERB_DECAY_HFLIMIT, reverb_preset.iDecayHFLimit);
+
+    alGenAuxiliaryEffectSlots(1, auxiliary_effect_slot);
+
+    if (alGetError() != AL_NO_ERROR) {
+        log_error("Failed to generate auxiliary effect slot");
+
+        quit(AUXILIARY_EFFECT_SLOT_GENERATION_FAILURE);
+    }
+
+    alAuxiliaryEffectSloti(auxiliary_effect_slot[0], AL_EFFECTSLOT_EFFECT, effect[0]);
 }
 
 void load_audio() {
     log_info("Load audio file in data buffer, and copy data buffer to source buffer");
 
     SF_INFO file_info;
+
+    // the file_info struct must be initialized before using it
+    memset (&file_info, 0, sizeof (file_info)) ;
 
     file = sf_open("Test.wav", SFM_READ, &file_info);
 
@@ -141,6 +200,9 @@ void load_audio() {
 
         quit(BIND_BUFFER_TO_SOURCE_FAILURE);
     }
+
+    // Bind effeclt slot to source
+    alSource3i(source[0], AL_AUXILIARY_SEND_FILTER, auxiliary_effect_slot[0], 0, AL_FILTER_NULL);
 }
 
 void play_audio() {
@@ -173,7 +235,11 @@ void quit(int failure_status) {
             free(data);
         case ALLOCATE_MEMORY_TO_DATA_BUFFER_FAILURE:
             sf_close(file);
+            alDeleteAuxiliaryEffectSlots(1, auxiliary_effect_slot);
         case AUDIO_FILE_OPEN_FAILURE:
+        case AUXILIARY_EFFECT_SLOT_GENERATION_FAILURE:
+            alDeleteEffects(1, effect);
+        case AUDIO_EFFECT_GENERATION_FAILURE:
             alDeleteSources(1, source);
         case AUDIO_SOURCE_GENERATION_FAILURE:
             alDeleteBuffers(1, buffer);
