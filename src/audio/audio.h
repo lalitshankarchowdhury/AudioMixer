b5de3af225ccff76aec5ab78a2c9413bd1817a00
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-05-15 15:50:59 +0530

    Update audio.h

--- src/audio/audio.h
+++ src/audio/audio.h
@@ -2,6 +2,7 @@
 
 #include <AL/al.h>
 #include <sndfile.h>
+#include <stdbool.h>
 
 typedef struct {
     SNDFILE* file;
@@ -13,11 +14,14 @@ typedef struct {
     ALenum format;
 } AudioClip;
 
-enum audio_failure_statuses { AUDIO_SUCCESS,
-    AUDIO_FAILURE };
+enum audio_failure_statuses {
+    AUDIO_FAILURE,
+    AUDIO_SUCCESS
+};
 
 int audioInitSubsystem();
 int audioLoadClip(AudioClip* clip, char const* clip_file_name);
 void audioUnloadClip(AudioClip* clip);
-int audioPlayClips(AudioClip* clips, int num_clips);
+int audioPlayClips(AudioClip clips[], int num_clips);
+bool audioIsClipPlaying(AudioClip* clip);
 void audioQuitSubsystem();
