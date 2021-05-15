74c278c1af2e0535763a151adfb5bf50c2562112
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-05-15 19:40:45 +0530

    Update audio.h

--- src/audio/audio.h
+++ src/audio/audio.h
@@ -12,7 +12,7 @@ typedef struct {
     int sample_rate;
     int channels;
     ALenum format;
-} AudioClip;
+} Clip;
 
 enum audio_failure_statuses {
     AUDIO_FAILURE,
@@ -20,8 +20,35 @@ enum audio_failure_statuses {
 };
 
 int audioInitSubsystem();
-int audioLoadClip(AudioClip* clip, char const* clip_file_name);
-void audioUnloadClip(AudioClip* clip);
-int audioPlayClips(AudioClip clips[], int num_clips);
-bool audioIsClipPlaying(AudioClip* clip);
+int audioLoadClip(Clip* clip, char const* clip_file_name);
+void audioUnloadClip(Clip* clip);
+int audioPlayClips(Clip clips[], int num_clips);
+bool audioIsClipPlaying(Clip* clip);
+void audioQuitSubsystem();
+#pragma once
+
+#include <AL/al.h>
+#include <sndfile.h>
+#include <stdbool.h>
+
+typedef struct {
+    SNDFILE* file;
+    ALuint buffer;
+    ALuint source;
+    long frames;
+    int sample_rate;
+    int channels;
+    ALenum format;
+} Clip;
+
+enum audio_failure_statuses {
+    AUDIO_FAILURE,
+    AUDIO_SUCCESS
+};
+
+int audioInitSubsystem();
+int audioLoadClip(Clip* clip, char const* clip_file_name);
+void audioUnloadClip(Clip* clip);
+int audioPlayClips(Clip clips[], int num_clips);
+bool audioIsClipPlaying(Clip* clip);
 void audioQuitSubsystem();
