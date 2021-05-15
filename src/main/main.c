df93e7328c414c2a077b3c10bf578250ea32b197
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-05-15 15:51:21 +0530

    Update main.c

--- src/main/main.c
+++ src/main/main.c
@@ -3,6 +3,7 @@
 #include "../log/log.h"
 
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -16,13 +17,21 @@ int main()
     assert(audioLoadClip(&clips[0], "Test1.wav") == AUDIO_SUCCESS);
     assert(audioLoadClip(&clips[1], "Test2.wav") == AUDIO_SUCCESS);
 
-    // Continuously play audio clips
-    while (true) {
-        audioPlayClips(clips, 2);
-    }
+    assert(audioPlayClips(clips, 2) == AUDIO_SUCCESS);
 
-    audioUnloadClip(&clips[0]);
-    audioUnloadClip(&clips[1]);
+    int clips_left_to_play = 2;
+
+    while (clips_left_to_play > 0) {
+        sleep(1);
+
+        for (int i = 0; i < 2; ++i) {
+            if (!audioIsClipPlaying(&clips[i])) {
+                audioUnloadClip(&clips[i]);
+
+                --clips_left_to_play;
+            }
+        }
+    }
 
     audioQuitSubsystem();
 
