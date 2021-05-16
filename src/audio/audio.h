4e31ac1508b4e957873a8bffe72ee4c146cf78d4
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-05-16 17:12:07 +0530

    Update audio.h
    Remove duplicated code

--- src/audio/audio.h
+++ src/audio/audio.h
@@ -25,30 +25,3 @@ void audioUnloadClip(Clip* clip);
 int audioPlayClips(Clip clips[], int num_clips);
 bool audioIsClipPlaying(Clip* clip);
 void audioQuitSubsystem();
-#pragma once
-
-#include <AL/al.h>
-#include <sndfile.h>
-#include <stdbool.h>
-
-typedef struct {
-    SNDFILE* file;
-    ALuint buffer;
-    ALuint source;
-    long frames;
-    int sample_rate;
-    int channels;
-    ALenum format;
-} Clip;
-
-enum audio_failure_statuses {
-    AUDIO_FAILURE,
-    AUDIO_SUCCESS
-};
-
-int audioInitSubsystem();
-int audioLoadClip(Clip* clip, char const* clip_file_name);
-void audioUnloadClip(Clip* clip);
-int audioPlayClips(Clip clips[], int num_clips);
-bool audioIsClipPlaying(Clip* clip);
-void audioQuitSubsystem();
